0825e8163ef4960bd2b72233e60452babc934f66
Bagus Rizki S <bagus.tho@gmail.com>
2021-09-21 14:19:51 +0700

    add confirm link

---
+++ netlify/functions/confirm.js
@@ -0,0 +1,23 @@
+const middy = require("@middy/core");
+const jsonBodyParser = require("@middy/http-json-body-parser");
+const httpErrorHandler = require("@middy/http-error-handler");
+const createError = require("http-errors");
+
+const count = 0;
+
+const baseHandler = async function (event, context, cb) {
+    console.log(event.body, " |||| ", count);
+    count++;
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            status: "OKE",
+        }),
+    };
+};
+
+const handler = middy(baseHandler)
+    .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
+    .use(httpErrorHandler()); // handles common http errors and returns proper responses
+
+module.exports = { handler };
